683f17af7d4d694d6185855f4d395318bf654762
All PGS 모든 프로그래머들 <kms0219kms@naver.com>
2021-04-14 13:17:07 +0900

    Create unsupported.js

---
+++ web/unsupported.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+module.exports = {
+    pathname: '/',
+    method: 'GET',
+    run: async (client, req, res, parsed) => {
+        res.writeHead(200, {
+            // 'strict-transport-security': 'max-age=86400; includeSubDomains; preload',
+            'content-type': 'text/html; charset=UTF-8'
+        });
+      if (req.headers['user-agent'] && (req.headers['user-agent'].includes('MSIE') || req.headers['user-agent'].includes('rv:11.0'))) {
+          res.writeHead(200, {
+              'Content-Type': "text/html; charset=UTF-8",
+              // 'strict-transport-security': 'max-age=86400; includeSubDomains; preload'
+          });
+          fs.readFile('./assets/html/unsupported.html', 'utf8', (err, data) => {
+              res.end(data);
+          });
+          return;
+      } else {
+          res.redirect(301, '/')
+    }
+}
